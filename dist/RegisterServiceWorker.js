50067df8f0df62d7164ab2cb60e7cdad455e908e
Andrew Noblet <andrewbnoblet@gmail.com>
2020-02-29 20:50:41 -0500

    chore(service-worker): update api to be more congruent

--- dist/RegisterServiceWorker.js
+++ dist/RegisterServiceWorker.js
@@ -1,8 +1,8 @@
-export const registerServiceWorker = ({ message, source, update }) => {
+export const registerServiceWorker = ({ installed, message, source }) => {
     import("workbox-window").then(({ Workbox }) => {
         if ("serviceWorker" in navigator) {
             const workbox = new Workbox(source);
-            workbox.addEventListener("installed", update);
+            workbox.addEventListener("installed", installed);
             workbox.addEventListener("message", message);
             workbox.register();
         }
--- src/RegisterServiceWorker.ts
+++ src/RegisterServiceWorker.ts
@@ -1,16 +1,16 @@
 export const registerServiceWorker = ({
+    installed,
     message,
-    source,
-    update
+    source
 }: {
+    installed: (event) => any;
     message: (event) => any;
     source: string;
-    update: (event) => any;
 }) => {
     import("workbox-window").then(({ Workbox }) => {
         if ("serviceWorker" in navigator) {
             const workbox = new Workbox(source);
-            workbox.addEventListener("installed", update);
+            workbox.addEventListener("installed", installed);
             workbox.addEventListener("message", message);
             workbox.register();
         }
