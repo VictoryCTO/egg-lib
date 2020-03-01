a9724a7fc607f53c417e42c9488639006048cf63
Andrew Noblet <andrewbnoblet@gmail.com>
2020-02-29 20:50:41 -0500

    chore(service-worker): update api to be more congruent

--- dist/RegisterServiceWorker.d.ts
+++ dist/RegisterServiceWorker.d.ts
@@ -1,5 +1,5 @@
-export declare const registerServiceWorker: ({ message, source, update }: {
+export declare const registerServiceWorker: ({ installed, message, source }: {
+    installed: (event: any) => any;
     message: (event: any) => any;
     source: string;
-    update: (event: any) => any;
 }) => void;
