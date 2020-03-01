dadffe714b4d3c00a05d0af9cf72d76acb811377
Andrew Noblet <andrewbnoblet@gmail.com>
2020-02-29 20:53:39 -0500

    chore(service-worker): make callbacks optional

--- dist/RegisterServiceWorker.d.ts
+++ dist/RegisterServiceWorker.d.ts
@@ -1,5 +1,5 @@
 export declare const registerServiceWorker: ({ installed, message, source }: {
-    installed: (event: any) => any;
-    message: (event: any) => any;
+    installed?: (event: any) => any;
+    message?: (event: any) => any;
     source: string;
 }) => void;
